3f399ebb1233e2168292608bf5d6112129639d02
Zaki Alfandy <zakialfandy30@gmail.com>
2025-02-28 18:19:25 +0700

    Update main.blade.php
    Tambah Fungsi Collapse pada menu data pegawai

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -281,14 +281,35 @@
         </a>
       </li><!-- End Dashboard Nav -->
 
-
       <li class="nav-item">
-        <a class="nav-link"  href="{{ route('index.index') }}">
-          <i class="bi bi-layout-text-window-reverse"></i><span>Table Data Pegawai</span>
-        </a> 
+        <a class="nav-link collapsed" data-bs-target="#tables-nav" data-bs-toggle="collapse" href="#" aria-expanded="false">
+          <i class="bi bi-layout-text-window-reverse"></i><span>Data Pegawai</span><i class="bi bi-chevron-down ms-auto"></i>
+        </a>
+        <ul id="tables-nav" class="nav-content collapse" data-bs-parent="#sidebar-nav" style="">
+          <li>
+            <a href="{{ route('index.index') }}">
+              <i class="bi bi-circle"></i><span>Biodata</span>
+            </a>
+          </li>
+          <li>
+            <a href="tables-data.html">
+              <i class="bi bi-circle"></i><span>Riwayat Jabatan</span>
+            </a>
+          </li>
+          <li>
+            <a href="tables-data.html">
+              <i class="bi bi-circle"></i><span>Riwayat Pendidikan</span>
+            </a>
+          </li>
+          <li>
+            <a href="tables-data.html">
+              <i class="bi bi-circle"></i><span>Dokumen Pendukung</span>
+            </a>
+          </li>
+        </ul>
       </li>
 
-
+    
 
     </ul>
 
