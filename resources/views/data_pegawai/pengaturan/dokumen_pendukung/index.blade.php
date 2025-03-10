88e720fbbf81741fa779386ae2cec7f4a6d610eb
irfanrestu <m.irfanrestu04@gmail.com>
2025-03-10 09:43:30 +0700

    Update tampilan dokumen pendukung untuk admin

--- resources/views/data_pegawai/pengaturan/dokumen_pendukung/index.blade.php
+++ resources/views/data_pegawai/pengaturan/dokumen_pendukung/index.blade.php
@@ -7,26 +7,40 @@
 
     <div class="row">
         <div class="col-lg-12">
-            @foreach ($kategoriDokumens as $kategori)
-                        <div class="mb-3">
-                            <label class="fw-bold">{{ $kategori->kategori_dokumen }}</label>
-                            <div>
-                                @php
-                                    $dokumen = $pegawai->dokumens
-                                        ->where('id_kategori_dokumen', $kategori->kategori_dokumen_id)
-                                        ->first();
-                                @endphp
-                                @if ($dokumen)
-                                    <a href="{{ asset($dokumen->file_dokumen) }}" target="_blank"
-                                        class="btn btn-sm btn-outline-primary d-flex justify-content-center align-items-center">
-                                        <i class="bi bi-file-earmark"></i> Lihat File
-                                    </a>
-                                @else
-                                    <span class="text-muted">-</span>
-                                @endif
-                            </div>
-                        </div>
-            @endforeach
+            <div class="card">
+                <div class="card-body">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Kategori Dokumen</th>
+                                <th>File</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($kategoriDokumens as $kategori)
+                                                        <tr>
+                                                            <td>{{ $kategori->kategori_dokumen }}</td>
+                                                            <td>
+                                                                @php
+                                                                    $dokumen = $pegawai->dokumens
+                                                                        ->where('id_kategori_dokumen', $kategori->kategori_dokumen_id)
+                                                                        ->first();
+                                                                @endphp
+                                                                @if ($dokumen)
+                                                                    <a href="{{ asset($dokumen->file_dokumen) }}" target="_blank"
+                                                                        class="btn btn-sm btn-outline-primary d-flex justify-content-center align-items-center">
+                                                                        <i class="bi bi-file-earmark"></i> Lihat File
+                                                                    </a>
+                                                                @else
+                                                                    <span class="text-muted">Belum ada file.</span>
+                                                                @endif
+                                                            </td>
+                                                        </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     </div>
 </div>
